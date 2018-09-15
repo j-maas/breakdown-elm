2d23f300961e48cb40e7189678814ad95d4a61a8
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-09-15 15:25:47 +0200

    Remove PWA features

--- static/setup.js
+++ static/setup.js
@@ -1,17 +1,3 @@
-// Register service worker
-// Code taken from https://developers.google.com/web/fundamentals/primers/service-workers/
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
-            // Registration was successful
-            console.debug('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.debug('ServiceWorker registration failed: ', err);
-        });
-    });
-}
-
 window.addEventListener('load', function () {
     var app = Elm.Main.init();
 })
