cfb4f9115d5294a8dff53086f6dd74c1d0d20a6e
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-10-02 15:52:01 +0200

    Add test for UpdateNewTask

--- tests/TestMain.elm
+++ tests/TestMain.elm
@@ -8,10 +8,17 @@ import Test exposing (..)
 
 suite : Test
 suite =
+    let
+        expectModelEquals a =
+            Tuple.first >> Expect.equal a
+    in
     describe "messages"
         [ test "NoOp keeps model the same" <|
             \_ ->
                 update NoOp initModel
-                    |> Tuple.first
-                    |> Expect.equal initModel
+                    |> expectModelEquals initModel
+        , test "UpdateNewTask saves the new task" <|
+            \_ ->
+                update (UpdateNewTask "i am an edit") initModel
+                    |> expectModelEquals { initModel | newTask = "i am an edit" }
         ]
