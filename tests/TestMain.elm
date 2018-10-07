fb20eac378db2036feb03aa6c03ddf0bde817a13
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-10-07 21:35:54 +0200

    Add tests for AddNewTask, refactor

--- tests/TestMain.elm
+++ tests/TestMain.elm
@@ -1,17 +1,14 @@
 module TestMain exposing (suite)
 
-import Expect
+import Expect exposing (Expectation)
 import Fuzz exposing (..)
 import Main exposing (Model, Msg(..), initModel, update)
+import Tasks
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    let
-        expectModelEquals a =
-            Tuple.first >> Expect.equal a
-    in
     describe "messages"
         [ test "NoOp keeps model the same" <|
             \_ ->
@@ -21,4 +18,40 @@ suite =
             \_ ->
                 update (UpdateNewTask "i am an edit") initModel
                     |> expectModelEquals { initModel | newTask = "i am an edit" }
+        , describe "AddNewTask"
+            [ test "saves valid task and resets new task" <|
+                \_ ->
+                    let
+                        rawAction =
+                            "I am a valid action."
+                    in
+                    testWithAction rawAction
+                        (\action ->
+                            update AddNewTask { initModel | newTask = rawAction }
+                                |> expectModelEquals
+                                    { initModel
+                                        | newTask = ""
+                                        , currentTasks = Tasks.appendTask action initModel.currentTasks
+                                    }
+                        )
+            , test "does not save valid task, but resets new task" <|
+                \_ ->
+                    update AddNewTask { initModel | newTask = "   " }
+                        |> expectModelEquals { initModel | newTask = "" }
+            ]
         ]
+
+
+expectModelEquals : Model -> ( Model, Cmd Msg ) -> Expectation
+expectModelEquals expected =
+    Tuple.first >> Expect.equal expected
+
+
+testWithAction : String -> (Tasks.Action -> Expectation) -> Expectation
+testWithAction rawAction test =
+    case Tasks.actionFromString rawAction of
+        Just action ->
+            test action
+
+        Nothing ->
+            Expect.fail "Expected test string to be valid action."
