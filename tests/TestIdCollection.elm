ee6110783fa80754afd565b6f26e4ac03d3eee5a
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-10-21 14:07:18 +0200

    Implement IdCollection

---
+++ tests/TestIdCollection.elm
@@ -0,0 +1,120 @@
+module TestIdCollection exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (..)
+import IdCollection exposing (Entry, IdCollection)
+import List.Extra as List
+import Test exposing (..)
+
+
+type CollectionTag
+    = Tag
+
+
+suite : Test
+suite =
+    let
+        emptyCollection =
+            IdCollection.empty Tag
+    in
+    describe "IdCollection"
+        [ describe "Build"
+            [ fuzz2 string string "appends item" <|
+                \first second ->
+                    emptyCollection
+                        |> IdCollection.append first
+                        |> IdCollection.append second
+                        |> toListWithoutIds
+                        |> Expect.equal [ first, second ]
+            , fuzz2 string string "appends and returns item" <|
+                \first second ->
+                    IdCollection.fromList Tag [ first ]
+                        |> IdCollection.appendAndGetEntry second
+                        |> Tuple.mapBoth .item toListWithoutIds
+                        |> Expect.equal ( second, [ first, second ] )
+            , fuzz (list string) "inits from list" <|
+                \list ->
+                    IdCollection.fromList Tag list
+                        |> toListWithoutIds
+                        |> Expect.equal list
+            , fuzz2 (list string) percentage "removes item by id" <|
+                \list offset ->
+                    testEntryInCollection
+                        (\index collection entry ->
+                            IdCollection.remove entry.id collection
+                                |> toListWithoutIds
+                                |> Expect.equal (List.removeAt index list)
+                        )
+                        list
+                        offset
+            , fuzz3 (list string) string percentage "sets item by id" <|
+                \list newItem offset ->
+                    testEntryInCollection
+                        (\index collection entry ->
+                            IdCollection.set newItem entry.id collection
+                                |> toListWithoutIds
+                                |> Expect.equal (List.setAt index newItem list)
+                        )
+                        list
+                        offset
+            , fuzz3 (list string) string percentage "updates item by id" <|
+                \list newItem offset ->
+                    testEntryInCollection
+                        (\index collection entry ->
+                            IdCollection.update (\item -> item ++ newItem) entry.id collection
+                                |> toListWithoutIds
+                                |> Expect.equal (List.updateAt index (\item -> item ++ newItem) list)
+                        )
+                        list
+                        offset
+            ]
+        , let
+            hasUniqueIds =
+                IdCollection.toList >> List.allDifferentBy (.id >> IdCollection.idToComparable)
+          in
+          fuzz (list string) "items have different ids" <|
+            \list ->
+                let
+                    collection =
+                        IdCollection.fromList Tag list
+                in
+                hasUniqueIds collection
+                    |> Expect.true ("Detected duplicate ids in:\n" ++ Debug.toString collection)
+        ]
+
+
+toListWithoutIds : IdCollection CollectionTag item -> List item
+toListWithoutIds =
+    IdCollection.toList >> List.map .item
+
+
+testEntryInCollection : (Int -> IdCollection CollectionTag item -> Entry CollectionTag item -> Expectation) -> List item -> Float -> Expectation
+testEntryInCollection test list offset =
+    IdCollection.fromList Tag list
+        |> (\collection ->
+                let
+                    index =
+                        offsetToIndex list offset
+
+                    collectionList =
+                        IdCollection.toList collection
+                in
+                case List.getAt index collectionList of
+                    Just entry ->
+                        test index collection entry
+
+                    Nothing ->
+                        if List.length list == 0 then
+                            -- If the list is empty, there is no entry. This doesn't mean that the function under test failed.
+                            Expect.pass
+
+                        else
+                            Expect.fail ("Error getting item by offset. Chosen index was:\n" ++ Debug.toString index)
+           )
+
+
+offsetToIndex : List item -> Float -> Int
+offsetToIndex list offset =
+    toFloat (List.length list - 1)
+        * offset
+        |> floor
