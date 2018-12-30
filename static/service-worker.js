94326c3a53697c910600e68cae0807ee45d0e865
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-12-30 14:28:25 +0100

    Fix service worker cache update

--- static/service-worker.js
+++ static/service-worker.js
@@ -13,8 +13,27 @@ function precache() {
 }
 
 self.addEventListener('fetch', function (evt) {
-    evt.respondWith(fromCache(evt.request));
-    evt.waitUntil(update(evt.request));
+    var cached = fromCache(evt.request);
+    evt.respondWith(
+        cached.catch(function () {
+            return update(evt.request)
+                .then(function () { return fromCache(evt.request); });
+        })
+    );
+
+    /*  We want to update the cache, even if we found something. But we
+     *  can't to it in respondWith, as that would delay the response.
+     *  However, we can simply swallow the error case, because it means
+     *  that we either did not hit the cache, which is handled in respondWith,
+     *  or we had an error fetching the resource, in which case we can do nothing.
+     */
+    evt.waitUntil(
+        cached.then(function () {
+            return update(evt.request)
+        }).catch(function () {
+            return undefined;
+        })
+    );
 });
 
 /** Open the cache where the assets were stored and search for the requested
@@ -31,8 +50,10 @@ function fromCache(request) {
 
 function update(request) {
     return caches.open(CACHE).then(function (cache) {
-        return fetch(request).then(function (response) {
-            return cache.put(request, response);
-        });
+        return fetch(request)
+            .then(function (response) {
+                return cache.put(request, response)
+                    .then(function () { return response; });
+            });
     });
 }
