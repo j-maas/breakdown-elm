451f9020e60d9efd14f81a942aa1dc46d1f239c0
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-03-05 11:14:53 -0400

    Add fuzz tests for Todo

--- tests/TestTodo.elm
+++ tests/TestTodo.elm
@@ -4,20 +4,32 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode as Decode
 import Test exposing (..)
-import Todo
+import Todo exposing (Todo)
 import Utils.NonEmptyString as NonEmptyString
 
 
 suite : Test
 suite =
     describe "Todo"
-        [ fuzz NonEmptyString.fuzzer "encoding and decoding results in same todo" <|
-            \_ ->
-                let
-                    todo =
-                        Todo.fromAction (NonEmptyString.build 'R' "estore me!")
-                in
+        [ fuzz NonEmptyString.fuzzer "returns same action it was created with" <|
+            \action ->
+                Todo.fromAction action
+                    |> Todo.action
+                    |> Expect.equal action
+        , fuzz2 todoFuzzer NonEmptyString.fuzzer "returns updated action" <|
+            \todo action ->
+                Todo.setAction action todo
+                    |> Todo.action
+                    |> Expect.equal action
+        , fuzz todoFuzzer "encoding and decoding results in same todo" <|
+            \todo ->
                 Todo.encode todo
                     |> Decode.decodeValue Todo.decoder
                     |> Expect.equal (Ok todo)
         ]
+
+
+todoFuzzer : Fuzzer Todo
+todoFuzzer =
+    Fuzz.map Todo.fromAction
+        NonEmptyString.fuzzer
