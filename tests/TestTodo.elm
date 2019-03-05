d203e140630ea2375ec21823c1207809baac3278
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-03-05 10:55:55 -0400

    Remove collections

--- tests/TestTodo.elm
+++ tests/TestTodo.elm
@@ -3,7 +3,6 @@ module TestTodo exposing (suite)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode as Decode
-import SelectCollection
 import StringFuzz exposing (nonblankStringFuzzer, whitespaceStringFuzzer)
 import Test exposing (..)
 import Todo
@@ -22,19 +21,6 @@ suite =
                 Todo.fromAction action
                     |> Todo.action
                     |> Expect.equal action
-        , test "creates todo with subtodos" <|
-            \_ ->
-                let
-                    child1 =
-                        Todo.fromAction (NonEmptyString.build 'I' "'m the first child.")
-
-                    child2 =
-                        Todo.fromAction (NonEmptyString.build 'I' "'m the second child.")
-                in
-                Todo.from (NonEmptyString.build 'I' "'m the parent.") (SelectCollection.init { current = [ child1, child2 ], done = [] })
-                    |> Todo.subtodos
-                    |> SelectCollection.mapToList SelectCollection.Current (\_ todo -> todo)
-                    |> Expect.equal [ child1, child2 ]
         , test "readActions returns action as String" <|
             \_ ->
                 Todo.fromAction (NonEmptyString.build 'R' "eturn me!")
@@ -46,20 +32,6 @@ suite =
                     |> Todo.setAction (NonEmptyString.build 'C' "hanged.")
                     |> Todo.readAction
                     |> Expect.equal "Changed."
-        , test "updates subtodos" <|
-            \_ ->
-                let
-                    child1 =
-                        Todo.fromAction (NonEmptyString.build 'I' "'m the first child.")
-
-                    child2 =
-                        Todo.fromAction (NonEmptyString.build 'I' "'m the second child.")
-                in
-                Todo.from (NonEmptyString.build 'R' "eplace my subtodos!") (SelectCollection.init { current = [ child1, child2 ], done = [] })
-                    |> Todo.setSubtodos (SelectCollection.init { current = [ child2 ], done = [] })
-                    |> Todo.subtodos
-                    |> SelectCollection.mapToList SelectCollection.Current (\_ todo -> todo)
-                    |> Expect.equal [ child2 ]
         , test "encoding and decoding results in same todo" <|
             \_ ->
                 let
